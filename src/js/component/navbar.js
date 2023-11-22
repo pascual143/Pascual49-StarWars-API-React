dcc32f73503a7a3e0c44c6d8b1f07a6b57f99860
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-11-22 19:13:18 +0000

    preparing navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfilePic from '../../img/startwars.png'
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src={ProfilePic} />
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<div class="dropdown">
+  						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  						  FAVORITES
+  						</button>
+  						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+  						 
+  						</div>
+					</div>
 				</Link>
 			</div>
 		</nav>
